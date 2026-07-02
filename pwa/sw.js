017d665466fc00aed824e7ec67de87d76b9d5b5a
Costin Stroie <costinstroie@eridu.eu.org>
2026-07-02 13:45:03 +0300

    Initial commit: IRIS project

---
+++ pwa/sw.js
@@ -0,0 +1,13 @@
+/* IRIS PWA service worker */
+const CACHE='iris-v18';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./support.js','./data/iris-data.js'];
+self.addEventListener('install',e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit=> hit || fetch(e.request).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{}); return res;
+    }).catch(()=> caches.match('./index.html')))
+  );
+});
